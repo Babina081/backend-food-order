2495436c8f70c07a2e312afc63e3784ef9fe8dc8
Babina081 <babitamoo333@gmail.com>
2024-07-06 17:14:31 +0545

    add made connection to mongodb using mongoose

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config.js");
+
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+//connection to mongodb
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING, {
+    dbName: process.env.MONGODB_DATABASE,
+  })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//connection to server (expressjs)
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
 });
