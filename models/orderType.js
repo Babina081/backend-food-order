86c4435103b9b1089ec7b37f45a9868cc803c7f0
Babina081 <babitamoo333@gmail.com>
2024-07-08 10:05:50 +0545

    remove unwanted models and routes

--- models/orderType.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const orderTypeSchema = mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-module.exports = mongoose.model("OrderType", orderTypeSchema);
--- models/restaurantService.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const restaurantServiceSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  icon: { type: String, required: true },
-});
-
-module.exports = mongoose.model("RestaurantService", restaurantServiceSchema);
